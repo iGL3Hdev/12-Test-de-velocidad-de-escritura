428d4d1118753843a3b72a7e74e8cc89770ba933
Your Name <you@example.com>
2024-08-06 23:32:46 +0200

    Pendiente Javascript

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
+const quoteSelection = document.getElementById("quote");
+const userInput = document.getElementById("quote-input");
+
+let quote = "";
+let time = 60;
+let timer = "";
+let mistakes = 0;
+
+const renderNewQuote = async () => {
+    const response = await fetch(quoteApiUrl);
+    let data = await response.json();
+    quote = data.content;
+
+
+    let arr = quote.split("").map((value) => {
+    return "<span class='quote-chars'>" + value + "<span>";
+    });
+    quoteSelection.innerHTML += arr.join("");
+};
+
+
 
 
 
